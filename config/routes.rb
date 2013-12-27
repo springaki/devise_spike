2e1539244c7a69650b586389974c1eb189ba530d
Aki <aki.onetime@gmail.com>
2013-12-27 21:52:35 +0900

    be rails g scaffold item name description:text

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DeviseSpike::Application.routes.draw do
+  resources :items
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
---
+++ spec/views/items/index.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "items/index" do
+  before(:each) do
+    assign(:items, [
+      stub_model(Item,
+        :name => "Name",
+        :description => "MyText"
+      ),
+      stub_model(Item,
+        :name => "Name",
+        :description => "MyText"
+      )
+    ])
+  end
+
+  it "renders a list of items" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+  end
+end
